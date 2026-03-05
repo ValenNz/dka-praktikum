1971c71d6dc492672523f257c337880ae2ec1e2f
Nuevalen Refitra Alswando <92833376+ValenNz@users.noreply.github.com>
2026-03-05 08:52:02 +0700

    update

--- modul1/latihan/latihan_kabisat.py
+++ modul1/latihan/latihan_kabisat.py
@@ -1,16 +1,5 @@
-tahun = int(input("Masukkan Tahun: "))
-
 def isKabisat(tahun):
-    if tahun % 400 == 0:
-        return True
-    elif tahun % 4 == 0 and tahun % 100 != 0:
-        return True
-    else:
-        return False
+    return tahun % 400 == 0 or (tahun % 4 == 0 and tahun % 100 != 0)
 
-hasil = isKabisat(tahun)
-
-if hasil:
-    print(f"{tahun} adalah kabisat")
-else:
-    print(f"{tahun} bukan kabisat")
+tahun = int(input())
+print(isKabisat(tahun))
